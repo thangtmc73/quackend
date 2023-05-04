7027ac48f16a79b01f6833d8c7a6c323d6e13df6
thangtm2 <thangtm2@vng.com.vn>
2023-05-05 00:08:07 +0700

    feat: separate function to connect database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,18 +4,26 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
 	"quackend/internal/model"
 )
 
 func main() {
 	post := model.Post{}
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "12345678", "127.0.0.1:3306", "blog")
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := connectDB()
 	db.First(&post)
 	if err == nil {
-		fmt.Println("database connected")
-		fmt.Printf("first post", post)
-	} else {
-		fmt.Println("connect database failed")
+		log.Printf("first post", post)
 	}
+}
+
+func connectDB() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "12345678", "127.0.0.1:3306", "blog")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err == nil {
+		log.Println("database connected")
+		return db, err
+	}
+	log.Println("connect database failed")
+	return nil, err
 }
